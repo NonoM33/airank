5f79e36b7ad60dbdd2fded944e6e1fa525d0318e
cosson renaud <renaudlemagicien@gmail.com>
2026-03-22 04:14:59 +0100

    fix: lazy PrismaClient init to avoid build-time DB connection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,6 +4,12 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// Lazy initialization - don't connect until first use
+export const prisma = new Proxy({} as PrismaClient, {
+  get(_target, prop) {
+    if (!globalForPrisma.prisma) {
+      globalForPrisma.prisma = new PrismaClient()
+    }
+    return (globalForPrisma.prisma as Record<string, unknown>)[prop as string]
+  }
+})
